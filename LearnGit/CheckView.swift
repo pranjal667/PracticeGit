c53398d3609121644eb432a93718560795af0cb3
pranjal667 <pranjalghimire667@gmail.com>
2023-08-11 10:40:22 +0545

    second commit

---
+++ LearnGit/CheckView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckView.swift
+//  LearnGit
+//
+//  Created by ebpearls on 11/08/2023.
+//
+
+import SwiftUI
+
+struct CheckView: View {
+    var body: some View {
+        Text("This is a check view!")
+    }
+}
+
+struct CheckView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckView()
+    }
+}
