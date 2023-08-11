28dfed8d3cc34d7e1c1a7174d34b726b113979f1
pranjal667 <pranjalghimire667@gmail.com>
2023-08-11 10:36:23 +0545

    first commit

---
+++ LearnGit/LearnGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitApp.swift
+//  LearnGit
+//
+//  Created by ebpearls on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
